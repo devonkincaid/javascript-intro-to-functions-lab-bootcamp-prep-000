85e047deb6d44cdbfd1e2806937383a6c9784e41
Devon Kincaid <tenaciousdk86@aol.com>
2017-12-19 19:49:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,5 +19,9 @@ function sayHiToGrandma(string){
     return "I can't hear you!"
   } if (string === string.toUpperCase()){
     return "YES INDEED!"
+  } if (string === "I love you, Grandma."){
+    return "I love you, too."
+  } else {
+    return "I'm tired"
   }
 }
